b60ccfc5603733db3994381b94648d9f4eba1c2b
wasp0l <unecorrigan@gmail.com>
2025-10-12 22:05:44 +0100

    Added call funtion

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
+from prompts import system_prompt
+from call_function import available_functions
+
 
 def main():
     load_dotenv()
@@ -17,7 +20,7 @@ def main():
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -36,17 +39,22 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-            config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
