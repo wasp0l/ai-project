b2f38fff2ce458b5b93a83f212a7d117e56b1610
wasp0l <unecorrigan@gmail.com>
2025-10-12 21:36:16 +0100

    commited tests changes

--- tests.py
+++ tests.py
@@ -1,14 +1,20 @@
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
     print(result)
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "tests.py")
     print(result)
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
+    result = run_python_file("calculator", "lorem.txt")
     print(result)
 
 
