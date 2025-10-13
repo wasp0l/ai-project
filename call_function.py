9688e3a28ed737612fae47744aac577903792ef4
wasp0l <unecorrigan@gmail.com>
2025-10-13 23:01:12 +0100

    Renamed write file to write file content

--- call_function.py
+++ call_function.py
@@ -1,12 +1,9 @@
 from google.genai import types
 
 from functions.get_files_info import schema_get_files_info
-
 from functions.get_file_content import schema_get_file_content
-
 from functions.run_python import schema_run_python_file
-
-from functions.write_file import schema_write_file
+from functions.write_file_content import schema_write_file
 
 available_functions = types.Tool(
     function_declarations=[
